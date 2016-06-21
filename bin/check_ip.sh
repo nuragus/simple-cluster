cec65e473fb6a5e597a1f5c7b63d7f46a4eb5ccf
Nur Agus <nuragus.linux@gmail.com>
2016-06-21 16:24:50 +0700

    check_ip.sh write to log

--- bin/check_ip.sh
+++ bin/check_ip.sh
@@ -1,19 +1,22 @@
+#!/bin/bash
 # check_ip.sh
 # Pinging reference IP to know network status
 
-#!/bin/bash
-
+# Read configuration file
 source ../settings.cfg;
 
+timestamp=$(date +%d%m%Y-%T);
 count=$( ping -c 5 -W 1 $reference_ip | grep ttl | wc -l );
 
 if [ $count -lt 3 ];
 then
-        echo "1"
-        RETVAL=1;
+  echo "1"
+  echo $timestamp": error ping reference ip" >> $log_directory/check_ip.log;
+  RETVAL=1;
 else
-        echo "0"
-        RETVAL=0;
+  echo "0"
+  echo $timestamp": success ping reference ip" >> $log_directory/check_ip.log;
+  RETVAL=0;
 fi
 
 exit $RETVAL;
