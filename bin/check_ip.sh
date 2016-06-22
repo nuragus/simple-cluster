a6e99fc58e3735af3dc899099d399cca1193d414
Nur Agus <nuragus.linux@gmail.com>
2016-06-22 21:53:07 +0700

    using printf instead of echo

--- bin/check_ip.sh
+++ bin/check_ip.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # check_ip.sh
 # Pinging reference IP to know network status
+# Reference IP is being pinged 5 times. If 3 or more, it is a success. This is to allow intermittance
 
 # Read configuration file
 source ../settings.cfg;
@@ -8,15 +9,14 @@ source ../settings.cfg;
 timestamp=$(date +%d%m%Y-%T);
 count=$( ping -c 5 -W 1 $reference_ip | grep ttl | wc -l );
 
-if [ $count -lt 3 ];
-then
-  echo "1"
-  echo $timestamp": error ping reference ip" >> $log_directory/check_ip.log;
-  RETVAL=1;
-else
-  echo "0"
-  echo $timestamp": success ping reference ip" >> $log_directory/check_ip.log;
+if [ $count -gt 2 ]; then
+  printf "%d\n" 0;
+  printf "%s : success ping reference ip\n" $timestamp >> $log_directory/check_ip.log;
   RETVAL=0;
+else
+  printf "%d\n" 1;
+  printf "%s : error ping reference ip\n" $timestamp >> $log_directory/check_ip.log;
+  RETVAL=1;
 fi
 
 exit $RETVAL;
--- bin/start_postgres.sh
+++ bin/start_postgres.sh
@@ -8,32 +8,31 @@
 # Read configuration file
 source ../settings.cfg;
 
-timestamp=$(date +%d%m%Y-%T);
 
-echo $timestamp": starting up database package" > $log_directory/startup.log;
+printf "%s : starting up database package" $timestamp >> $log_directory/startup.log;
 
 # Mounting postgres
+timestamp=$(date +%d%m%Y-%T);
 mount $postgres_mount > /dev/null 2>&1;
 if [ $? == "0" ]; then
-  echo $timestamp": success mounting postgres" >> $log_directory/startup.log;
+  printf "%s : success mounting postgres" $timestamp >> $log_directory/startup.log;
 else
-  echo $timestamp": failed mounting postgres" >> $log_directory/startup.log;
+  printf "%s : failed mounting postgres" $timestmp >> $log_directory/startup.log;
 fi
 
 # Activate floating IP
-ip_exec="ifconfig "$floating_device" "$floating_ip" netmask"$floating_netmask;
-#echo $ip_exec;
+timestamp=$(date +%d%m%Y-%T);
 ifconfig $floating_device $floating_ip netmask $floating_netmask > /dev/null;
 if [ $? == "0" ]; then
-  echo $timestamp": success activating floating ip" >> $log_directory/startup.log;
+  printf "%s : success activating floating ip" $timestamp >> $log_directory/startup.log;
 else
-  echo $timestamp": failed activating floating ip" >> $log_directory/startup.log;
+  printf "%s : failed activating floating ip" $timestamp >> $log_directory/startup.log;
 fi
 
 # Starting up database
 /bin/su -l postgres -c 'source ~/pg_env.sh; pg_ctl -D $PGDATA start' > /dev/null
 if [ $? == "0" ]; then
-  echo $timestamp": success starting up database" >> $log_directory/startup.log;
+  printf "%s : success starting up database" $timestamp >> $log_directory/startup.log;
 else
-  echo $timestamp": failed starting up database" >> $log_directory/startup.log;
+  printf "%s : failed starting up database" $timestamp >> $log_directory/startup.log;
 fi
