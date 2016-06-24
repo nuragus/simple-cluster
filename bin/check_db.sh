9fc5149178a8fcf0c0c1fe12c0bc4f40663eefc1
Nur Agus <nuragus.linux@gmail.com>
2016-06-24 22:57:22 +0700

    check_db.sh: disable echo exec path

--- bin/check_db.sh
+++ bin/check_db.sh
@@ -3,7 +3,6 @@
 this_host=`hostname`
 
 source $main_directory/settings.cfg;
-echo $pg_exec;
 
 count=$( for i in {1..5}; do $pg_exec/psql -U job-bpm -d job-bpm -h $floating_ip -c 'select COUNT(*) FROM bpm_config'; done | grep row | wc -l ) > /dev/null;
 timestamp=$(date +%d%m%Y-%T);
