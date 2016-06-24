369c97e459f25e4f131b96571a665b3f48a5f444
Nur Agus <nuragus.linux@gmail.com>
2016-06-24 23:24:11 +0700

    reloc_here.sh: initiate

--- bin/reloc_here.sh
+++ bin/reloc_here.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 
-source settings.cfg;
+source $main_directory/settings.cfg;
 
 checkip=`$main_directory/check_ip.sh`;
-checkowner=`cat /bpm/scripts/i_was_owner`;
+checkowner=`cat $db_directory/i_was_owner.db`;
 thishost=`hostname`;
 timestamp=$(date +%d%m%Y-%T);
+logfile="$log_directory/reloc.log";
+start_exec="$bin_directory/start_postgres.sh";
 
-echo $timestamp >> /bpm/scripts/reloc.log;
+echo $timestamp >> $log_directory/reloc.log;
 
 if [ $checkip == "0" ]; then
   if [ $checkowner == "0" ]; then
     echo $timestamp": i was the owner; sleeping for 10 check cycles before mounting and startup postgres" >> /bpm/scripts/reloc.log;
     for i in {1..10}; do
       if [ `/bpm/scripts/check_db.sh` == "0" ]; then
-        echo $timestamp": postgres is running! probably at the other node" >> /bpm/scripts/reloc.log;
+        echo $timestamp": postgres is running! probably at the other node" >> $logfile;
         exit 0;
       fi
-	echo $timestamp": still sleeping... preventing split brain" >> /bpm/scripts/reloc.log;
+	echo $timestamp": still sleeping... preventing split brain" >> $logfile;
 	sleep 1;
     done;
     if [ `/bpm/scripts/check_db.sh` == "1" ]; then
-       echo $timestamp": after sleeping long enough, postgres still down; starting up here" >> /bpm/scripts/reloc.log;
+       echo $timestamp": after sleeping long enough, postgres still down; starting up here" >> $logfile;
        exec "/bpm/scripts/start_db.sh";
-       echo $timestamp": now i am the owner of postgres" >> /bpm/scripts/reloc.log;
+       echo $timestamp": now i am the owner of postgres" >> $logfile
     fi
   else
-    echo "i was not the owner, seems i can start now" >> /bpm/scripts/reloc.log;
+    echo "i was not the owner, seems i can start now" >> $logfile;
     if [ $thishost == "node1" ]; then
-      ssh root@node2 /bpm/scripts/cleanup_db.sh;
+      ssh root@node2 $main_directory/stop_postgres.sh;
     elif [ $thishost == "node2" ]; then
-      ssh root@node1 /bpm/scripts/cleanup_db.sh;
-    fi	
-    exec "/bpm/scripts/start_db.sh";
+      ssh root@node1 $main_directory/stop_postgres.sh;
+    if
+    exec $start_exec;
   fi
 else
   echo "my network is problem; do nothing to prevent split brain; need human intervention" >> /bpm/scripts/reloc.log;
