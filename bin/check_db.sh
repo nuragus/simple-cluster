95010e528ed71029efa68152a51bca546bd0399b
Nur Agus <nuragus.linux@gmail.com>
2016-06-24 22:56:00 +0700

    check_db.sh seems okay

--- bin/check_db.sh
+++ bin/check_db.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-source $main_directory/settings.cfg;
-
 this_host=`hostname`
-count=$( for i in {1..10}; do nc -w 1 -z -v $floating_ip 5432; done | grep succeeded | wc -l );
 
-if [ $count -lt 5 ];
+source $main_directory/settings.cfg;
+echo $pg_exec;
+
+count=$( for i in {1..5}; do $pg_exec/psql -U job-bpm -d job-bpm -h $floating_ip -c 'select COUNT(*) FROM bpm_config'; done | grep row | wc -l ) > /dev/null;
+timestamp=$(date +%d%m%Y-%T);
+
+
+if [ $count -lt 3 ];
 then
   echo $timestamp >> /$log_directory/checkdb.log;
   echo "the database is not running" >> /$log_directory/checkdb.log;
   echo "1"
-        RETVAL=1;
+  RETVAL=1;
 else
-        owner=`ssh root@$floating_ip "hostname"`;
-	echo "0";
+  owner=`ssh root@$floating_ip "hostname"`;
+  echo "0";
 	if [ $owner == $this_host ];
 	then
 		echo "0" > /$db_directory/i_was_owner.db;
 	else
-		echo "1" > /$db_directory/i_was_owner.db;
+		echo "1" > /$db_directory/i_was_ownerdb;
 	fi
-  echo $timestamp >> /$log_directory/checkdb.log;
-  echo "the database is running at:"$owner >> /$log_directory/checkdb.log;
+  echo $timestamp >> /$db_directory/checkdb.log;
+  echo "the database is running at:"$owner >> /$db_directory/checkdb.log;
         RETVAL=0;
 fi
 
